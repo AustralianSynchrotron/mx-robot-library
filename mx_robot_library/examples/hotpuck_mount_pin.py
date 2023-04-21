c238278804cddffd082aa87097d3e57fbfe5ccb0
Jacob Oldfield <oldfielj@ansto.gov.au>
2023-04-21 16:01:35 +1000

    Add hot puck code examples

---
+++ mx_robot_library/examples/hotpuck_mount_pin.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+from mx_robot_library.client import Client
+from mx_robot_library.schemas.common.path import RobotPaths
+
+# Create a new client instance
+robot = Client(host="192.168.0.0", readonly=False)
+
+# Try to mount a pin
+pin = robot.utils.get_pin(id=5, puck=1)
+msg = robot.trajectory.hot_puck.mount(pin=pin)
+
+# Wait until operation starts
+while robot.status.state.path != RobotPaths.HOTPUCK_PUT:
+    sleep(0.5)
+
+# Wait until operation ends
+while robot.status.state.path != RobotPaths.UNDEFINED:
+    sleep(0.5)
+
+# Check that the pin on the goni, matches expectations
+assert robot.status.state.goni_pin == pin
